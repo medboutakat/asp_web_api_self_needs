ce02f01b5bc8bf29d3bb3de71e9e5fcba7de750f
med <boutakatmohamed@gmail.com>
2019-01-11 13:54:09 +0100

    Mohamed boutakat

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http; 
+using System.Web;
+
+namespace WebApiSelfHost
+{
+  
+
+    public class Program
+    {
+        static void Main()
+        {
+
+
+            Console.WriteLine("Hello user: please enter tenant name or enter for default name ");
+            string name=Console.ReadLine();
+            Console.Title = "hELLO / " + name;
+
+            string baseAddress = "http://localhost:9000/";
+
+            // Start OWIN host 
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                // Create HttpCient and make a request to api/values 
+                HttpClient client = new HttpClient();
+
+                var response = client.GetAsync(baseAddress + "api/values").Result;
+
+                Console.WriteLine(response);
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,33 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace WebApiSelfHost
+{
+
+    public class Startup
+    {
+        // This code configures Web API. The Startup class is specified as a type
+        // parameter in the WebApp.Start method.
+        public void Configuration(IAppBuilder appBuilder)
+        {
+             
+            HttpConfiguration config = new HttpConfiguration();
+ 
+            // Configure Web API for self-host. 
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+             
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
